c278c35e87c35efea7e3def010cf28d1736aa0fc
WebDevMullins <webdevmullins@gmail.com>
2023-10-09 15:01:35 -0500

    add createLogo, update tests

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import inquirer from 'inquirer'
+import { createLogo } from './lib/createLogo.js'
 import { prompts } from './lib/prompts.js'
 
 function init() {
-	inquirer.prompt(prompts).then((response) => console.log(response))
+	inquirer
+		.prompt(prompts)
+		.then((response) => {
+			createLogo(response)
+		})
+		.catch((err) => {
+			console.log(err)
+		})
 }
 
 init()
